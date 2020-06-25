7ff6d3828978eb427c73f947ad5ab1dc43b7822c
Yue A. Qi <36769119+YueAndyQi@users.noreply.github.com>
2020-06-24 22:55:16 -0400

    Update gotobed_bam.sh

--- gotobed_bam.sh
+++ gotobed_bam.sh
@@ -3,4 +3,4 @@
 module load samtools
 module load bedtools
 
-samtools view -b 7_PC9_DMSO_6h_B1_S40_R1_001_bbduk.genome.sorted.deduplicated.bam | bedtools bamtobed -i stdin > 7_PC9_DMSO_6h_B1_S40_R1_001_bbduk.genome.sorted.deduplicated.bed
+samtools view -b your_bbduk.genome.sorted.deduplicated.bam | bedtools bamtobed -i stdin > your_bbduk.genome.sorted.deduplicated.bed
