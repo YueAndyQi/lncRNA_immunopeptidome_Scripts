cfaafa93478be7c0640cd73f60bf7888e9ffcc2d
Yue A. Qi <36769119+YueAndyQi@users.noreply.github.com>
2020-06-26 12:25:47 -0400

    Update bed-intersect.sh

--- bed-intersect.sh
+++ bed-intersect.sh
@@ -2,6 +2,8 @@
 
 module load bedtools
 
-bedtools intersect -a your_bbduk.genome.sorted.deduplicated.bed -b /path/to/lncs/lncipedia_5_2_hc_hg38.bed_renamed.bed > your-lnc-intersect.bed
+samplebed=$1
+
+bedtools intersect -a $samplebed -b /path/to/lncs/lncipedia_5_2_hc_hg38.bed_renamed.bed > intersect.bed
 
 # Note: You can change the names and paths of the BED files here.
